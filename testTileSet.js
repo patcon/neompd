d43905c90beee899ae1c725694c998612560d518
Ates Goral <ates.g@myplanetdigital.com>
2013-11-05 14:39:50 -0500

    Use LESS variables for column/gutter configuration

--- testTileSet.js
+++ testTileSet.js
@@ -5,7 +5,7 @@ define([], function () {
 
     return {
         '#articles/test01': '<a href="#articles/test01">One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.</a>',
-        '#articles/test02': '<a href="#articles/test02">He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment.</a>',
+        '#articles/test02': '<a class="-double" href="#articles/test02">He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment.</a>',
         '#articles/test03': '<a href="#articles/test03">His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What\'s happened to me? " he thought. It wasn\'t a dream.</a>',
         '#articles/test04': '<a href="#articles/test04">His room, a proper human room although a little too small, lay peacefully between its four familiar walls.</a>',
         '#articles/test05': '<a href="#articles/test05">A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame.</a>',
